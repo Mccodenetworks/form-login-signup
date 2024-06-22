c54a66bf527aba269f3b7f9b7fbb053a5bfc59fd
Mccodenetworks <150978779+Mccodenetworks@users.noreply.github.com>
2024-06-22 06:59:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password");
+let login = document.getElementById("login");
+
+
+
+function validate(){
+login.innerHTML = "Sign Up";
+    
+}
+
